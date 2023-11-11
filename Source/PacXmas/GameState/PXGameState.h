b74df11cec815b3ec7cc744c119d8ebef189de4f
Arek <arek0006@gmail.com>
2023-11-11 15:50:23 +0100

    Added folders for GameState

---
+++ Source/PacXmas/GameState/PXGameState.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2023 Santa Claus. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "PXGameState.generated.h"
+
+UCLASS()
+class PACXMAS_API APXGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
