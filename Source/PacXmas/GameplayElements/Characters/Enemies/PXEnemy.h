b02366c1c246fbb2ef6bb735196a891947fbb67c
Arek <arek0006@gmail.com>
2023-11-11 16:36:12 +0100

    Added classes for Enemy

---
+++ Source/PacXmas/GameplayElements/Characters/Enemies/PXEnemy.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2023 Santa Claus. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PXEnemy.generated.h"
+
+UCLASS()
+class PACXMAS_API APXEnemy : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APXEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
