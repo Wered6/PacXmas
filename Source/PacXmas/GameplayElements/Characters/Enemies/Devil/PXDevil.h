36f36872d6820a131b93600193e52ee6c2beda23
Arek <arek0006@gmail.com>
2023-11-13 13:39:41 +0100

    Added classes for Boy, Girl and Devil

---
+++ Source/PacXmas/GameplayElements/Characters/Enemies/Devil/PXDevil.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Santa Claus. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PacXmas/GameplayElements/Characters/Enemies/PXEnemy.h"
+#include "PXDevil.generated.h"
+
+UCLASS()
+class PACXMAS_API APXDevil : public APXEnemy
+{
+	GENERATED_BODY()
+};
---
+++ Source/PacXmas/GameplayElements/Characters/Player/Girl/PXGirl.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Santa Claus. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PacXmas/GameplayElements/Characters/Player/PXPlayer.h"
+#include "PXGirl.generated.h"
+
+UCLASS()
+class PACXMAS_API APXGirl : public APXPlayer
+{
+	GENERATED_BODY()
+};
