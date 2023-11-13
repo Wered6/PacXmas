5322bcb135d7029deaef7f5c5fa1f34f2539149e
Arek <arek0006@gmail.com>
2023-11-13 13:07:40 +0100

    Added DataAssets classes for boy

---
+++ Source/PacXmas/DataAssets/Characters/Player/Boy/PXBoyDA.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Santa Claus. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PacXmas/DataAssets/Characters/PXCharacterDA.h"
+#include "PXBoyDA.generated.h"
+
+UCLASS()
+class PACXMAS_API UPXBoyDA : public UPXCharacterDA
+{
+	GENERATED_BODY()
+};
