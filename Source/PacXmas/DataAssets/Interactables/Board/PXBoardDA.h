a1cdf5dd2fce72d3abb92e3eae76c4adac77b67d
Arek <arek0006@gmail.com>
2023-11-23 19:16:25 +0100

    Added data assets for interactables and then board

---
+++ Source/PacXmas/DataAssets/Interactables/Board/PXBoardDA.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Santa Claus. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PacXmas/DataAssets/Interactables/PXInteractablesDA.h"
+#include "PXBoardDA.generated.h"
+
+UCLASS()
+class PACXMAS_API UPXBoardDA : public UPXInteractablesDA
+{
+	GENERATED_BODY()
+};
