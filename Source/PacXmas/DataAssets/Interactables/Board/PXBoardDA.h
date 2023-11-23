4a310186f22c2bfd43ac1cee082f4a8ddfa17fb1
Arek <arek0006@gmail.com>
2023-11-23 21:53:57 +0100

    Created pockets for sprites of PXBoard in PXBoardDA

--- Source/PacXmas/DataAssets/Interactables/Board/PXBoardDA.h
+++ Source/PacXmas/DataAssets/Interactables/Board/PXBoardDA.h
@@ -3,11 +3,25 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "PacXmas/DataAssets/Interactables/PXInteractablesDA.h"
+#include "Engine/DataAsset.h"
 #include "PXBoardDA.generated.h"
 
+class UPaperSprite;
+
 UCLASS()
-class PACXMAS_API UPXBoardDA : public UPXInteractablesDA
+class PACXMAS_API UPXBoardDA : public UDataAsset
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Sprites")
+	UPaperSprite* Sprite0{nullptr};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Sprites")
+	UPaperSprite* Sprite1{nullptr};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Sprites")
+	UPaperSprite* Sprite2{nullptr};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Sprites")
+	UPaperSprite* Sprite3{nullptr};
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Sprites")
+	UPaperSprite* Sprite4{nullptr};
 };
