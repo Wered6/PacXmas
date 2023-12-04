dc34d910237d2f55f12917e628eebff0f12d2541
Arek <arek0006@gmail.com>
2023-12-04 14:37:35 +0100

    Created PXProjectilePudding class

---
+++ Source/PacXmas/GameplayElements/Projectiles/Pudding/PXProjectilePudding.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Santa Claus. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PacXmas/GameplayElements/Projectiles/PXProjectile.h"
+#include "PXProjectilePudding.generated.h"
+
+UCLASS()
+class PACXMAS_API APXProjectilePudding : public APXProjectile
+{
+	GENERATED_BODY()
+};
