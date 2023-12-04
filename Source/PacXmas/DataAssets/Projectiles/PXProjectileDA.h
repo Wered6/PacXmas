950806e9e0a40e156b93b17382da86d8dfb95d09
Arek <arek0006@gmail.com>
2023-12-04 15:08:03 +0100

    Added PXProjectileDA class

---
+++ Source/PacXmas/DataAssets/Projectiles/PXProjectileDA.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Santa Claus. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PXProjectileDA.generated.h"
+
+UCLASS()
+class PACXMAS_API UPXProjectileDA : public UDataAsset
+{
+	GENERATED_BODY()
+};
