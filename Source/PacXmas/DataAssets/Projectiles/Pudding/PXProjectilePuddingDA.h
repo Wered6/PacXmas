d3fa7033a339fad9fee6b09b430ebcf823ad49ae
Arek <arek0006@gmail.com>
2023-12-04 15:09:34 +0100

    Added PXProjectilePuddingDA class

---
+++ Source/PacXmas/DataAssets/Projectiles/Pudding/PXProjectilePuddingDA.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Santa Claus. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PacXmas/DataAssets/Projectiles/PXProjectileDA.h"
+#include "PXProjectilePuddingDA.generated.h"
+
+UCLASS()
+class PACXMAS_API UPXProjectilePuddingDA : public UPXProjectileDA
+{
+	GENERATED_BODY()
+};
