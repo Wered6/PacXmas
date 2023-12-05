9be51c23d07db2e3031cc8b9e2a4d3f2eed998c1
Arek <arek0006@gmail.com>
2023-12-05 12:45:36 +0100

    Added socket for sprite PXProjectilePuddingDA and filled it with sprite

--- Source/PacXmas/DataAssets/Projectiles/PXProjectileDA.h
+++ Source/PacXmas/DataAssets/Projectiles/PXProjectileDA.h
@@ -6,8 +6,14 @@
 #include "Engine/DataAsset.h"
 #include "PXProjectileDA.generated.h"
 
+class UPaperSprite;
+
 UCLASS()
 class PACXMAS_API UPXProjectileDA : public UDataAsset
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Sprites")
+	UPaperSprite* Sprite{nullptr};
 };
