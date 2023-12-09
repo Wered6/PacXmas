13f9080246e34235047df0cfe707ef2c753f5f01
Arek <arek0006@gmail.com>
2023-12-09 13:34:35 +0100

    Added PXSplashedPuddingDA for PXSplashedPudding

---
+++ Source/PacXmas/DataAssets/VisualEffects/PXSplashedPuddingDA.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Santa Claus. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PXSplashedPuddingDA.generated.h"
+
+UCLASS()
+class PACXMAS_API UPXSplashedPuddingDA : public UDataAsset
+{
+	GENERATED_BODY()
+};
