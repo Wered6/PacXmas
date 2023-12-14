8e78ef97c88d5fd143aec4b85bc9cce418433ec4
Arek <arek0006@gmail.com>
2023-12-14 19:28:13 +0100

    Added code for initializng and opening widgets in PXMenu class

--- Source/PacXmas/UI/Menu/PXMenu.cpp
+++ Source/PacXmas/UI/Menu/PXMenu.cpp
@@ -2,3 +2,49 @@
 
 
 #include "PXMenu.h"
+#include "PacXmas/Utilities/CustomLogs/PXCustomLogs.h"
+#include "Widgets/ChooseName/PXChooseNameWidget.h"
+#include "Widgets/EndGame/PXEndGameWidget.h"
+#include "Widgets/Highscores/PXHighscoresWidget.h"
+#include "Widgets/StartGame/PXStartGameWidget.h"
+
+void UPXMenu::InitializeWidgets()
+{
+	StartGameWidget = InitializeWidget(StartGameWidgetClass);
+	ChooseNameWidget = InitializeWidget(ChooseNameWidgetClass);
+	HighscoresWidget = InitializeWidget(HighscoresWidgetClass);
+	EndGameWidget = InitializeWidget(EndGameWidgetClass);
+}
+
+void UPXMenu::OpenStartGameWidget() const
+{
+	OpenWidget(StartGameWidget);
+}
+
+void UPXMenu::OpenChooseNameWidget() const
+{
+	OpenWidget(ChooseNameWidget);
+}
+
+void UPXMenu::OpenHighscoresWidget() const
+{
+	OpenWidget(HighscoresWidget);
+}
+
+void UPXMenu::OpenEndGameWidget() const
+{
+	OpenWidget(EndGameWidget);
+
+	// todo add if to activate widget on loose and on win
+}
+
+void UPXMenu::OpenWidget(UUserWidget* Widget) const
+{
+	if (!Widget)
+	{
+		UE_LOG(LogWidget, Warning, TEXT("UPXMenu::OpenWidget|Widget is nullptr"))
+		return;
+	}
+
+	Widget->AddToViewport();
+}
--- Source/PacXmas/UI/Menu/PXMenu.h
+++ Source/PacXmas/UI/Menu/PXMenu.h
@@ -3,11 +3,59 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
+#include "PacXmas/Utilities/CustomLogs/PXCustomLogs.h"
 #include "PXMenu.generated.h"
+
+class UPXEndGameWidget;
+class UPXHighscoresWidget;
+class UPXChooseNameWidget;
+class UPXStartGameWidget;
 
 UCLASS(Blueprintable)
 class PACXMAS_API UPXMenu : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	void InitializeWidgets();
+
+	void OpenStartGameWidget() const;
+	void OpenChooseNameWidget() const;
+	void OpenHighscoresWidget() const;
+	void OpenEndGameWidget() const;
+
+private:
+	template <typename T>
+	T* InitializeWidget(const TSubclassOf<T>& WidgetClass)
+	{
+		if (!WidgetClass)
+		{
+			UE_LOG(LogWidget, Warning, TEXT("T* InitializeWidget|WidgetClass is nullptr"))
+			return nullptr;
+		}
+
+		return CreateWidget<T>(GetWorld(), WidgetClass);
+	}
+
+	void OpenWidget(UUserWidget* Widget) const;
+
+	UPROPERTY(EditDefaultsOnly, Category="Menu|StartGame")
+	TSubclassOf<UPXStartGameWidget> StartGameWidgetClass;
+	UPROPERTY()
+	UPXStartGameWidget* StartGameWidget{nullptr};
+
+	UPROPERTY(EditDefaultsOnly, Category="Menu|ChooseName")
+	TSubclassOf<UPXChooseNameWidget> ChooseNameWidgetClass;
+	UPROPERTY()
+	UPXChooseNameWidget* ChooseNameWidget{nullptr};
+
+	UPROPERTY(EditDefaultsOnly, Category="Menu|Highscores")
+	TSubclassOf<UPXHighscoresWidget> HighscoresWidgetClass;
+	UPROPERTY()
+	UPXHighscoresWidget* HighscoresWidget{nullptr};
+
+	UPROPERTY(EditDefaultsOnly, Category="Menu|EndGame")
+	TSubclassOf<UPXEndGameWidget> EndGameWidgetClass;
+	UPROPERTY()
+	UPXEndGameWidget* EndGameWidget{nullptr};
 };
