ca30625c862e89b2618514a8912ce7967d191856
Arek <arek0006@gmail.com>
2023-12-29 12:36:30 +0100

    Created empty PXPlayerMovementComponent

---
+++ Source/PacXmas/GameplayElements/Characters/Player/MovementComponent/PXPlayerMovementComponent.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Santa Claus. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "PXPlayerMovementComponent.generated.h"
+
+UCLASS()
+class PACXMAS_API UPXPlayerMovementComponent : public UPawnMovementComponent
+{
+	GENERATED_BODY()
+};
