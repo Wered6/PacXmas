976ceec754cca8566b91aa37c1ab2fe954706ad1
Arek <arek0006@gmail.com>
2023-12-31 13:36:38 +0100

    Movement Component for PXPlayer works fine but without queueing turning

--- Source/PacXmas/GameplayElements/Characters/Player/MovementComponent/PXPlayerMovementComponent.cpp
+++ Source/PacXmas/GameplayElements/Characters/Player/MovementComponent/PXPlayerMovementComponent.cpp
@@ -2,3 +2,132 @@
 
 
 #include "PXPlayerMovementComponent.h"
+#include "PacXmas/GameplayElements/Characters/Player/PXPlayer.h"
+
+UPXPlayerMovementComponent::UPXPlayerMovementComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	TileSize = DefaultTileSize;
+	MovementSpeed = DefaultMovementSpeed;
+	bIsMoving = false;
+	DesiredDirection = FVector::ZeroVector;
+	CurrentDirection = DesiredDirection;
+}
+
+void UPXPlayerMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TargetLocation = PawnOwner->GetActorLocation();
+}
+
+void UPXPlayerMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                               FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!PawnOwner || !UpdatedComponent || ShouldSkipUpdate(DeltaTime))
+	{
+		return;
+	}
+
+	if (!bIsMoving && !DesiredDirection.IsZero() && CanMoveInDirection(DesiredDirection))
+	{
+		bIsMoving = true;
+		TargetLocation = UpdatedComponent->GetComponentLocation() + DesiredDirection * TileSize;
+		CurrentDirection = DesiredDirection;
+	}
+
+	if (bIsMoving)
+	{
+		MoveInDirection(CurrentDirection, DeltaTime);
+	}
+
+	if (HasReachedTileBorder())
+	{
+		PawnOwner->SetActorLocation(TargetLocation);
+		bIsMoving = false;
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("ActorLocation: %s"), *PawnOwner->GetActorLocation().ToString())
+	UE_LOG(LogTemp, Warning, TEXT("DesiredDirection: %s"), *DesiredDirection.ToString())
+	UE_LOG(LogTemp, Warning, TEXT("TargetLocation: %s"), *TargetLocation.ToString())
+	UE_LOG(LogTemp, Warning, TEXT("CurrentDirection: %s"), *CurrentDirection.ToString())
+	if (bIsMoving)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("bIsMoving: true"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("bIsMoving: false"))
+	}
+}
+
+void UPXPlayerMovementComponent::SetDesiredDirection(const FVector& NewDirection)
+{
+	DesiredDirection = NewDirection;
+}
+
+bool UPXPlayerMovementComponent::CanMoveInDirection(const FVector& Direction) const
+{
+	if (!PawnOwner)
+	{
+		UE_LOG(LogActor, Warning, TEXT("UPXPlayerMovementComponent::CanMoveInDirection|PawnOwner is nullptr"))
+		return false;
+	}
+
+	const APXPlayer* PXPlayer = Cast<APXPlayer>(PawnOwner);
+
+	FHitResult HitResult;
+	const FVector StartPosition = PXPlayer->GetActorLocation();
+	const FVector EndPosition = StartPosition + Direction * MoveCheckDistance;
+
+	FCollisionShape CollisionShape;
+	CollisionShape.SetBox(FVector3f(PXPlayer->GetScaledBoxExtent()));
+	FCollisionQueryParams CollisionQueryParams;
+
+	bool bHit = GetWorld()->SweepSingleByChannel(
+		HitResult,
+		StartPosition,
+		EndPosition,
+		FQuat::Identity,
+		ECC_GameTraceChannel11,
+		CollisionShape,
+		CollisionQueryParams
+	);
+
+	return !bHit;
+}
+
+
+bool UPXPlayerMovementComponent::HasReachedTileBorder() const
+{
+	if (!PawnOwner)
+	{
+		UE_LOG(LogActor, Warning, TEXT("UPXPlayerMovementComponent::HasReachedTileBorder|PawnOwner is nullptr"))
+		return false;
+	}
+
+	const FVector CurrentLocation = PawnOwner->GetActorLocation();
+	const FVector DirectionToTarget = (TargetLocation - CurrentLocation).GetSafeNormal();
+	const FVector CurrentMoveDirection = CurrentDirection.GetSafeNormal();
+
+	if (FVector::DotProduct(DirectionToTarget, CurrentMoveDirection) < 0)
+	{
+		return true;
+	}
+
+	const float DistanceToTarget = FVector::Dist(CurrentLocation, TargetLocation);
+	return DistanceToTarget < BorderProximityThreshold;
+}
+
+void UPXPlayerMovementComponent::MoveInDirection(const FVector& Direction, const float DeltaTime)
+{
+	const FVector ActorLocation = PawnOwner->GetActorLocation();
+	const FVector NewLocation = ActorLocation + (MovementSpeed * DeltaTime * Direction);
+
+	const FVector MoveDistance = NewLocation - ActorLocation;
+	const FRotator ActorRotation = PawnOwner->GetActorRotation();
+
+	MoveUpdatedComponent(MoveDistance, ActorRotation, true);
+}
--- Source/PacXmas/GameplayElements/Characters/Player/MovementComponent/PXPlayerMovementComponent.h
+++ Source/PacXmas/GameplayElements/Characters/Player/MovementComponent/PXPlayerMovementComponent.h
@@ -10,4 +10,35 @@ UCLASS()
 class PACXMAS_API UPXPlayerMovementComponent : public UPawnMovementComponent
 {
 	GENERATED_BODY()
+
+public:
+	UPXPlayerMovementComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+public:
+	void SetDesiredDirection(const FVector& NewDirection);
+
+private:
+	bool CanMoveInDirection(const FVector& Direction) const;
+	bool HasReachedTileBorder() const;
+	void MoveInDirection(const FVector& Direction, const float DeltaTime);
+
+	FVector DesiredDirection;
+	FVector CurrentDirection;
+	FVector NextDesiredDirection;
+	FVector TargetLocation;
+	float TileSize;
+	bool bIsMoving;
+	float MovementSpeed;
+
+	static constexpr float DefaultTileSize{32.f};
+	static constexpr float DefaultMovementSpeed{300.f};
+	static constexpr float MoveCheckDistance{1.5f};
+	static constexpr float BorderProximityThreshold{1.f};
 };
