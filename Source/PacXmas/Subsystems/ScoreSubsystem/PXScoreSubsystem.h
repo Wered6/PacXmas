74b182db7e899ad87e474efb1629b8d5cfc62abd
Arek <arek0006@gmail.com>
2024-01-06 13:59:02 +0100

    Added empty ScoreSubsystem

---
+++ Source/PacXmas/Subsystems/ScoreSubsystem/PXScoreSubsystem.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Santa Claus. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "PXScoreSubsystem.generated.h"
+
+UCLASS()
+class PACXMAS_API UPXScoreSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+};
