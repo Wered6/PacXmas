475072f47898751157292d2cd0bb6a8821b2c9a2
Arek <arek0006@gmail.com>
2024-01-10 21:40:27 +0100

    Empty movcomps for enemies and player

---
+++ Source/PacXmas/GameplayElements/Characters/Enemies/MovementComponent/PXEnemyMovementComponent.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Smoking Carrots. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PacXmas/GameplayElements/Characters/MovementComponent/PXCharacterMovementComponent.h"
+#include "PXEnemyMovementComponent.generated.h"
+
+UCLASS()
+class PACXMAS_API UPXEnemyMovementComponent : public UPXCharacterMovementComponent
+{
+	GENERATED_BODY()
+};
---
+++ Source/PacXmas/GameplayElements/Characters/Player/MovementComponent/PXPlayerMovementComponent.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Smoking Carrots. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PacXmas/GameplayElements/Characters/MovementComponent/PXCharacterMovementComponent.h"
+#include "PXPlayerMovementComponent.generated.h"
+
+UCLASS()
+class PACXMAS_API UPXPlayerMovementComponent : public UPXCharacterMovementComponent
+{
+	GENERATED_BODY()
+};
