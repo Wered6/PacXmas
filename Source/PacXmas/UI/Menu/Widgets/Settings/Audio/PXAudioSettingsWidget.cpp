146e459276e7b5e38d0642a01e92dab69634f3e4
Arek <arek0006@gmail.com>
2024-01-29 08:07:42 +0100

    Added simple audio settings, slider works and musicvolumeminus button

---
+++ Source/PacXmas/UI/Menu/Widgets/Settings/Audio/PXAudioSettingsWidget.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2023 Smoking Carrots. All rights reserved.
+
+
+#include "PXAudioSettingsWidget.h"
---
+++ Source/PacXmas/UI/Menu/Widgets/Settings/Audio/PXAudioSettingsWidget.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Smoking Carrots. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PXAudioSettingsWidget.generated.h"
+
+UCLASS()
+class PACXMAS_API UPXAudioSettingsWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
