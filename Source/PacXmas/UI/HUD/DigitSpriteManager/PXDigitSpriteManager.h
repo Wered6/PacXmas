fbb315c0c42322c517d1b53d1e6cba9689fc7b97
Arek <arek0006@gmail.com>
2024-02-02 12:08:59 +0100

    Added empty manager for digit sprites and life sprites in HUD

---
+++ Source/PacXmas/UI/HUD/DigitSpriteManager/PXDigitSpriteManager.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Smoking Carrots. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PXDigitSpriteManager.generated.h"
+
+UCLASS(Blueprintable)
+class PACXMAS_API UPXDigitSpriteManager : public UObject
+{
+	GENERATED_BODY()
+};
---
+++ Source/PacXmas/UI/HUD/LifeSpriteManager/PXLifeSpriteManager.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Smoking Carrots. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PXLifeSpriteManager.generated.h"
+
+UCLASS(Blueprintable)
+class PACXMAS_API UPXLifeSpriteManager : public UObject
+{
+	GENERATED_BODY()
+};
