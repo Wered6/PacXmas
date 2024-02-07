54d66d15db3a008aceadf449d2ba86c976f6d065
Arek <arek0006@gmail.com>
2024-02-07 11:24:29 +0100

    Added empty PXScorePopup class c++ and bp

---
+++ Source/PacXmas/UI/HUD/ScorePupup/PXScorePopup.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Smoking Carrots. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PXScorePopup.generated.h"
+
+UCLASS()
+class PACXMAS_API UPXScorePopup : public UUserWidget
+{
+	GENERATED_BODY()
+};
