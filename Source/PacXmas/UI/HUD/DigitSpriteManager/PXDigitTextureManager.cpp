dda858053c52327c996aa13d1081d549e99f2622
Arek <arek0006@gmail.com>
2024-02-09 17:28:24 +0100

    Added GetSignTexture function in PXDigitTextureManager

--- Source/PacXmas/UI/HUD/DigitSpriteManager/PXDigitTextureManager.cpp
+++ Source/PacXmas/UI/HUD/DigitSpriteManager/PXDigitTextureManager.cpp
@@ -8,10 +8,33 @@ UTexture2D* UPXDigitTextureManager::GetDigitTexture(const uint8_t Digit)
 {
 	if (!DigitTextures.IsValidIndex(Digit))
 	{
-		UE_LOG(LogDigitTextureManager, Warning,
+		UE_LOG(LogManager, Warning,
 		       TEXT("UPXDigitTextureManager::GetDigitTexture|%d is out of DigitTextures array bounds"), Digit)
 		return nullptr;
 	}
 
 	return DigitTextures[Digit];
+}
+
+UTexture2D* UPXDigitTextureManager::GetSignTexture(const bool bPositive)
+{
+	if (!SignTextures.IsValidIndex(0))
+	{
+		UE_LOG(LogManager, Warning,
+		       TEXT("UPXDigitTextureManager::GetSignTexture|Index 0 is out of SignTextures array bounds"))
+		return nullptr;
+	}
+	if (!SignTextures.IsValidIndex(1))
+	{
+		UE_LOG(LogManager, Warning,
+		       TEXT("UPXDigitTextureManager::GetSignTexture|Index 1 is out of SignTextures array bounds"))
+		return nullptr;
+	}
+
+	if (!bPositive)
+	{
+		return SignTextures[0];
+	}
+
+	return SignTextures[1];
 }
--- Source/PacXmas/UI/HUD/DigitSpriteManager/PXDigitTextureManager.h
+++ Source/PacXmas/UI/HUD/DigitSpriteManager/PXDigitTextureManager.h
@@ -12,8 +12,11 @@ class PACXMAS_API UPXDigitTextureManager : public UObject
 
 public:
 	UTexture2D* GetDigitTexture(const uint8_t Digit);
+	UTexture2D* GetSignTexture(const bool bPositive);
 
 private:
 	UPROPERTY(EditDefaultsOnly, Category="Digit Textures")
 	TArray<UTexture2D*> DigitTextures;
+	UPROPERTY(EditDefaultsOnly, Category="Sign Textures")
+	TArray<UTexture2D*> SignTextures;
 };
