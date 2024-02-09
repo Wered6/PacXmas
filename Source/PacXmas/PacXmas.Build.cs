a61e091e3ebbe72111c3f42ef4110a22d848f24c
Arek <arek0006@gmail.com>
2024-02-09 17:07:23 +0100

    Added modules UMG, SLate and SlateCore

--- Source/PacXmas/PacXmas.Build.cs
+++ Source/PacXmas/PacXmas.Build.cs
@@ -8,7 +8,7 @@ public class PacXmas : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D", "UMG", "Slate", "SlateCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
