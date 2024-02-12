4355d060a79e4b711d350e1e61a7fc82d558276a
Arek <arek0006@gmail.com>
2024-02-12 13:32:38 +0100

    Added empty bp and c++ classes for HUDOverlay

---
+++ Source/PacXmas/UI/HUD/HUDOverlay/PXHUDOverlay.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2023 Smoking Carrots. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PXHUDOverlay.generated.h"
+
+UCLASS()
+class PACXMAS_API UPXHUDOverlay : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
